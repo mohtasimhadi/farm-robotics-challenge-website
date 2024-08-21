58bbde392be117e0b3f02f4c74fe97674abee5cf
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-08-21 17:10:42 -0500

    logo added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+import { AppBar, Toolbar, Button } from '@mui/material';
 
 const Navbar: React.FC = () => {
   return (
     <AppBar position="sticky" style={{ backgroundColor: 'green' }}>
       <Toolbar>
-        <Typography variant="h6" style={{ flexGrow: 1 }}>
-          My Application
-        </Typography>
+        <img 
+          src="/logo192.png" 
+          alt="Logo" 
+          style={{ height: 40, marginRight: 'auto' }} 
+        />
         <Button color="inherit" href="#home">Home</Button>
         <Button color="inherit" href="#path-following">Path Following</Button>
         <Button color="inherit" href="#plant-counting">Plant Counting</Button>
