94378926206cbb89e9170a48fc698d8bc2f04f5c
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-08-21 09:58:39 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,30 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import React from 'react';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import MainSection from './components/MainSection';
-import PathFollowing from './components/PathFollowing';
-import TeamSection from './components/TeamSection';
-// Import other components
 
-const App = () => {
+function App() {
   return (
     <div className="App">
-      <Navbar />
-      <main>
-        <section id="main-section">
-          <MainSection />
-        </section>
-        <section id="path-following-section">
-          <PathFollowing />
-        </section>
-        <section id="the-team-section">
-          <TeamSection />
-        </section>
-      </main>
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
