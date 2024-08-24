93dab48d123d5267dc2834335a9f743d3cffcbd0
Mohtasim Hadi Rafi <mohtasimhadi@gmail.com>
2024-08-24 01:22:28 -0500

    biography changed

--- src/components/TeamSection.tsx
+++ src/components/TeamSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography, Link } from '@mui/material';
-import { LinkedIn as LinkedInIcon, Language as LanguageIcon } from '@mui/icons-material';
+import { LinkedIn as LinkedInIcon, Language as LanguageIcon, School as SchoolIcon } from '@mui/icons-material';
 
 const TeamSection: React.FC = () => {
   return (
@@ -9,7 +9,7 @@ const TeamSection: React.FC = () => {
       flexDirection="column" 
       alignItems="center" 
       p={3}
-      sx={{ backgroundColor: '#E8EBE0'}} // Added backgroundColor style
+      sx={{ backgroundColor: '#E8EBE0'}}
     >
       <Typography variant="h3" gutterBottom align="center">
         Meet the Team
@@ -20,6 +20,7 @@ const TeamSection: React.FC = () => {
         justifyContent="center" 
         alignItems="center"
       >
+        {/* Team Member 1 */}
         <Box 
           width="25%" 
           p={2} 
@@ -36,7 +37,9 @@ const TeamSection: React.FC = () => {
             }} 
           />
           <Typography variant="body1">Hamid Habib Syed</Typography>
-          <Typography variant="body2">hhs0015@auburn.edu</Typography>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography variant="body2" sx={{ marginLeft: '4px' }}>hhs0015@auburn.edu</Typography>
+          </Box>
           <Box>
             <Link href="https://hamidsyed298.github.io/" target="_blank" style={{ marginRight: '8px' }}>
               <LanguageIcon />
@@ -46,6 +49,8 @@ const TeamSection: React.FC = () => {
             </Link>
           </Box>
         </Box>
+
+        {/* Team Member 2 */}
         <Box 
           width="25%" 
           p={2} 
@@ -62,7 +67,9 @@ const TeamSection: React.FC = () => {
             }} 
           />
           <Typography variant="body1">Faraz Ahmed</Typography>
-          <Typography variant="body2">fza0070@auburn.edu</Typography>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography variant="body2" sx={{ marginLeft: '4px' }}>fza0070@auburn.edu</Typography>
+          </Box>
           <Box>
             <Link href="" target="_blank" style={{ marginRight: '8px' }}>
               <LanguageIcon />
@@ -72,6 +79,8 @@ const TeamSection: React.FC = () => {
             </Link>
           </Box>
         </Box>
+
+        {/* Team Member 3 */}
         <Box 
           width="25%" 
           p={2} 
@@ -88,7 +97,9 @@ const TeamSection: React.FC = () => {
             }} 
           />
           <Typography variant="body1">Mesbahul Maruf</Typography>
-          <Typography variant="body2">mzm0411@auburn.edu</Typography>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography variant="body2" sx={{ marginLeft: '4px' }}>mzm0411@auburn.edu</Typography>
+          </Box>
           <Box>
             <Link href="" target="_blank" style={{ marginRight: '8px' }}>
               <LanguageIcon />
@@ -98,6 +109,8 @@ const TeamSection: React.FC = () => {
             </Link>
           </Box>
         </Box>
+
+        {/* Team Member 4 */}
         <Box 
           width="25%" 
           p={2} 
@@ -114,9 +127,14 @@ const TeamSection: React.FC = () => {
             }} 
           />
           <Typography variant="body1">Mohtasim Hadi Rafi</Typography>
-          <Typography variant="body2">mzr0167@auburn.edu</Typography>
+          <Box display="flex" justifyContent="center" alignItems="center">
+            <Typography variant="body2" sx={{ marginLeft: '4px' }}>mzr0167@auburn.edu</Typography>
+          </Box>
           <Box>
-            <Link href="moho.blog" target="_blank" style={{ marginRight: '8px' }}>
+            <Link href="https://scholar.google.com/citations?user=ih7NQy8AAAAJ&hl=en&oi=ao" target="_blank" style={{ marginRight: '8px' }}>
+              <SchoolIcon />
+            </Link>
+            <Link href="https://moho.blog" target="_blank" style={{ marginRight: '8px' }}>
               <LanguageIcon />
             </Link>
             <Link href="https://www.linkedin.com/in/mohtasimhadi/" target="_blank">
